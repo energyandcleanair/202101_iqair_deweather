321f08cc658e90ba27df748fa7270c75990791c8
Hubert Thieriot <hubert.thieriot@gmail.com>
2021-01-28 01:18:00 +0000

    adding fire

--- deweather.R
+++ deweather.R
@@ -20,11 +20,9 @@ meas.clean <- meas
 f <- file.path("results","data", "meas_dew.RDS")
 
 if(!file.exists(f)){
-
   # There was very little difference between lag of one or two days
   # although a bit more so in China (lag2<lag1)
   lags <- c(0)
-
   meas.dews <- lapply(lags, function(lag){
     deweather(meas=meas.clean,
               poll="pm25",
@@ -45,6 +43,35 @@ if(!file.exists(f)){
 }else{
   meas.dew <- readRDS(f)
 }
+
+f.fire <- file.path("results","data", "meas_dew_fire.RDS")
+
+if(!file.exists(f.fire)){
+  # There was very little difference between lag of one or two days
+  # although a bit more so in China (lag2<lag1)
+  lags <- c(0)
+  meas.dews <- lapply(lags, function(lag){
+    deweather(meas=meas.clean,
+              poll="pm25",
+              output=c("trend"),
+              add_pbl=T,
+              upload_results=F,
+              add_fire=T,
+              # training_end_anomaly = "2019-12-31",
+              lag=lag
+    ) %>%
+      mutate(lag=!!lag)
+  })
+
+  meas.dew.fire <- do.call("bind_rows",
+                      meas.dews)
+
+  saveRDS(meas.dew.fire, f.fire)
+
+}else{
+  meas.dew.fire <- readRDS(f.fire)
+}
+
 
 meas.dew <- meas.dew %>% filter(lag==0)
 
