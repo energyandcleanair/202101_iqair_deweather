0c92a1f6001629e27d4f5f359b535f45a303b920
Hubert Thieriot <hubert@energyandcleanair.org>
2021-02-24 17:20:00 +0800

    -plot with fewer cities

--- deweather.R
+++ deweather.R
@@ -27,10 +27,12 @@ meas.clean <- meas
 meas.dew.trend <- calc.deweather(meas.clean, mode="trend", fire_mode=NULL,  use_cache=T)
 meas.dew.anomaly <- calc.deweather(meas.clean, mode="anomaly",fire_mode=NULL, use_cache=T)
 meas.dew.trend.fire.trajectory <- calc.deweather(meas.clean, mode="trend", fire_mode="trajectory", use_cache=T)
+meas.dew.anomaly.fire.trajectory <- calc.deweather(meas.clean, mode="anomaly", fire_mode="trajectory", use_cache=T)
 
 
 change.trend <- utils.table_change(meas.clean, "trend", meas.dew.trend)
 change.anomaly <- utils.table_change(meas.clean, "anomaly", meas.dew.anomaly)
+change.anomaly.lockdown <- utils.table_change_lockdown(meas.clean, "anomaly", meas.dew.anomaly)
 change.trend.fire.trajectory  <- utils.table_change(meas.clean, "trend", meas.dew.trend.fire.trajectory)
 
 
@@ -53,8 +55,20 @@ write_csv(change.trend.fire.trajectory, file.path("results","data","change.trend
 plot.trend(meas.dew.trend, change.trend, filename="ts.trend.png")
 plot.trend(meas.dew.trend.fire.trajectory, change.trend.fire.trajectory, filename="ts.trend.fire.trajectory.png")
 plot.observed_vs_predicted(meas.dew.anomaly, change=change.anomaly, filename="ts.anomaly.full.png")
+
 plot.ts(meas.dew = meas.dew.anomaly, mode="anomaly")
 plot.ts(meas.dew = meas.dew.anomaly, mode="anomaly", weather.fire=weather.fire.trajectory, fire_mode="trajectory")
+
+cities_short <- c("Wuhan","Beijing","Singapore")
+plot.ts(meas.dew = meas.dew.anomaly %>% filter(location_id %in% cities_short),
+        mode="anomaly", weather.fire=weather.fire.trajectory, fire_mode="trajectory",
+        ncol=1,
+        height=10,
+        width=8,
+        facet_scales = "free_y",
+        filename="ts.anomaly.trajectory.fire.short.png")
+
+
 plot.weather.fire(weather.fire.trajectory, fire_mode="trajectory", running_days=30)
 plot.weather.fire(weather.fire.oriented, fire_mode="oriented", running_days=30)
 plot.weather.fire(weather.fire.circular, fire_mode="circular", running_days=30)
@@ -64,3 +78,8 @@ plot.change(change.trend, modes=c("dew","observed"), filename="bar.trend.observe
 plot.change(change.trend, modes=c("dew"), filename="bar.trend.png")
 plot.change(change.anomaly, modes=c("dew","observed"), filename="bar.anomaly.observed.png")
 plot.change(change.anomaly, modes=c("dew"), filename="bar.anomaly.png")
+
+
+# Change lockdown not lockdown --------------------------------------------
+
+
